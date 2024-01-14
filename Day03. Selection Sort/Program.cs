e85c87e03aa75d7ff6ba880dc220411481d76dde
GR1240 <ruslan1240@mail.ru>
2024-01-15 01:37:50 +0300

    added documentation and changes

--- Day03. Selection Sort/Program.cs
+++ Day03. Selection Sort/Program.cs
@@ -53,3 +53,15 @@
 
 // // _____________________________________________________________________________________
 
+using static Sorting;
+using static Infrastructure;
+
+// int[] array = CreateArray(10);
+// Show(array);
+// SortSelection(array);
+// Show(array);
+
+10.CreateArray()
+  .Show()
+  .SortSelection()
+  .Show();
